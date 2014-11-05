ba45984268819fbe60bee3c9141193250e7e8d57
Daniele Salvatore <daniele.salvatore@fao.org>
2014-11-05 18:05:13 +0100

    Metadata Editor integration

--- scripts/mains/createdataset.js
+++ scripts/mains/createdataset.js
@@ -1,9 +1,11 @@
 /*global require*/
-// relative or absolute path of Components' main.js
-require([
-    '../../submodules/fenix-ui-DSDEditor/js/paths'
-], function (Editor) {
 
+
+// relative or absolute path of Components' main.js
+
+define(['module'], function (module) {
+
+    var userConfig = module.config();
 
     var override = {
 
@@ -13,30 +15,59 @@ require([
         'bootstrap': '../../node_modules/bootstrap/dist/js/bootstrap.min'
     };
 
+    require(['../../submodules/fenix-ui-metadata-editor/js/paths',
+        '../../submodules/fenix-ui-DSDEditor/js/paths'
+    ], function (MetadataEditor, Editor) {
+
+        // NOTE: This setTimeout() call is used because, for whatever reason, if you make
+        //       a 'require' call in here or in the Cart without it, it will just hang
+        //       and never actually go fetch the files in the browser. There's probably a
+        //       better way to handle this, but I don't know what it is.
+        setTimeout(function () {
+
+            /*
+             @param: prefix of Components paths to reference them also in absolute mode
+             @param: paths to override
+             @param: options passed in to override defaults
+             @param: callback function
+             */
+            MetadataEditor.initialize('../../submodules/fenix-ui-metadata-editor/js', override, userConfig, function () {
+
+                Editor.initialize('../../submodules/fenix-ui-DSDEditor/js', override, function () {
+                    require([
+                        'fx-editor/start',
+                        'fenix-ui-topmenu/main',
+                        'fx-DSDEditor/start'
+                    ], function (StartUp, TopMenu, E) {
+
+                        new StartUp().init(userConfig);
+
+                        new TopMenu({
+                            url: 'json/fenix-ui-topmenu_config.json', active: "createdataset"
+                        });
+
+                        E.init();
+
+                        window.setTimeout(function (){
+                            $('#DSDEditorContainer').hide();
+                        }, 2000);
+
+                        document.body.addEventListener("fx.editor.finish", function (e) {
+                            console.log(e.detail.data)
+
+                            $('#metadataEditorContainer').hide();
+                            $('#DSDEditorContainer').show();
 
 
-// NOTE: This setTimeout() call is used because, for whatever reason, if you make
-// a 'require' call in here or in the Cart without it, it will just hang
-// and never actually go fetch the files in the browser. There's probably a
-// better way to handle this, but I don't know what it is.
-    setTimeout(function () {
-        /*
-         @param: prefix of Components paths to reference them also in absolute mode
-         @param: paths to override
-         @param: callback function
-         */
-        Editor.initialize('../../submodules/fenix-ui-DSDEditor/js', override, function () {
-            require([
-                'fx-DSDEditor/start'
-                , 'fenix-ui-topmenu/main'
-            ], function (E,TopMenu) {
-                E.init();
+                        }, false);
 
-                new TopMenu({
-                    url: 'json/fenix-ui-topmenu_config.json', active: "createdataset"
+                    });
+
                 });
 
             });
-        });
-    }, 0);
+
+        }, 0);
+    });
+
 });
