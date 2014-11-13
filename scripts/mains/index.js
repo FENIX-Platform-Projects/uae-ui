2bccbd2147ebff60c57df7088e532988b186f91d
Nicola Selleri <nicola.selleri@fao.org>
2014-11-13 18:42:27 +0100

    United Arab Emirates theme applied

--- scripts/mains/index.js
+++ scripts/mains/index.js
@@ -6,20 +6,23 @@ requirejs.config({
         "host": '../index/host',
         "jquery": "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min",
         "bootstrap": "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min",
-        "fenix-ui-topmenu": '../components/fenix-ui-topmenu'
+        "fenix-ui-topmenu": '../components/fenix-ui-topmenu',
+        "highcharts" : '//code.highcharts.com/highcharts'
     },
 
     shim: {
         "bootstrap": {
             deps: ["jquery"]
+        },
+        "highcharts": {
+            deps: ["jquery"]
         }
     }
 });
 
-
 require(['host', 'bootstrap', 'domReady!'], function (Host) {
 
     var host = new Host();
-    host.initFenixComponent()
+    host.initFenixComponent();
 
 });
