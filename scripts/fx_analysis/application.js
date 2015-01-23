8a4ad131bd32610027290b8c74f9baa50386c000
danielefenix <daniele.salvatore@fao.org>
2015-01-23 16:11:38 +0100

    .

--- scripts/fx_analysis/application.js
+++ scripts/fx_analysis/application.js
@@ -1,9 +1,22 @@
+/*global define, TweenLite*/
+
 define([
+    'jquery',
     'fx-ana/start',
     'fx-cat-br/start',
     'fx-filter/start',
+    'fx-menu/start',
     'amplify'
-], function (Analysis, Catalog, Filter) {
+], function ($, Analysis, Catalog, Filter, Menu) {
+
+    'use strict';
+
+    var s = {
+        CATALOG_CONTAINER: '#catalogContainer',
+        FILTER_CONTAINER: '#filterContainer',
+        FILTER_HOLDER : '#filterHolder',
+        FILTER_BTN : '#get_filter_values'
+    };
 
     function Application(options) {
 
@@ -14,9 +27,10 @@ define([
     }
 
     Application.prototype.init = function () {
-        this.initAnimation();
+
         this.bindEventListeners();
         this.initFxComponents();
+        this.initAnimation();
     };
 
     Application.prototype.initFxComponents = function () {
@@ -25,13 +39,13 @@ define([
 
         this.catalog = new Catalog().init({
             BLANK_FILTER: 'config/submodules/catalog/uae-catalog-blank-filter.json',
-            container: document.querySelector('#catalogContainer'),
+            container: document.querySelector(s.CATALOG_CONTAINER),
             results: {
                 actions: {
                     SELECT_RESOURCE: {
                         event: 'open',
-                        labels : {
-                            EN : 'Open Resource'
+                        labels: {
+                            EN: 'Open Resource'
                         }
                     }
                 }
@@ -40,31 +54,301 @@ define([
         });
 
         this.filter = new Filter();
+        this.filter.init({
+            container: s.FILTER_CONTAINER,
+            plugin_prefix: '../../../submodules/fenix-ui-filter/'
+        });
+
+        //TopMenu
+        this.topMenu = new Menu({
+            url: 'config/submodules/fx-menu/uae-topmenu-config.json',
+            active: "createdataset"
+        });
     };
+
+    Application.prototype.addToFilter = function (resource) {
+        console.log(resource)
+        //this.filter.add(this.getFilterConfiguration(resource));
+    };
+
 
     Application.prototype.bindEventListeners = function () {
 
-        var self = this;
+        amplify.subscribe('fx.widget.catalog.open', this, function (resource) {
 
-        amplify.subscribe('fx.widget.catalog.open', function(){
-            console.log("open catalog")
-            //that.closeOverlay();
+            $(s.CATALOG_CONTAINER).hide();
+            this.analysis.getData(resource, $.proxy(this.addToFilter, this))
 
-            //here
+        });
 
-        })
+        $(s.FILTER_BTN).on('click', $.proxy(this.filterResource , this));
 
 
-/*        *//*Event triggered by the catalog when "Open Data" button is clicked*//*
-        $(this.o.selectors.EVENTS_LISTENERS).on('analyze', function (e, payload) {
+        /*        */
+        /*Event triggered by the catalog when "Open Data" button is clicked*/
+        /*
+         $(this.o.selectors.EVENTS_LISTENERS).on('analyze', function (e, payload) {
 
-            that.getData(payload, $.proxy(that.addItemToDesk, that))
-        });*/
+         that.getData(payload, $.proxy(that.addItemToDesk, that))
+         });*/
+
+
+        /*
+         * FILTER
+         *
+         *
+         *         $('body').on("fx.host.component.ready", function (event, properties) {
+
+         //The host can set now the domain
+         fc.setDomain("FirstComponent", [{"label": "l10", "value":"v10"}, {"label": "l20", "value":"v20"}]);
+         });
+         * */
 
 
     };
 
+    Application.prototype.filterResource = function () {
+       console.log(this.filter.getValues())
+    };
 
+    Application.prototype.getFilterConfiguration = function (resource) {
+
+        var modules = [
+            {
+                "containerType": "container1",
+                "title": "Container1Title",
+                "components": [
+                    {
+                        "componentType": "component1",
+//                    "source": [
+//                        {"label": "l1", "value": "v1"},
+//                        {"label": "l2", "value": "v2"}
+//                    ],
+                        //"type":"radiobuttongroup",
+                        //"domain_type" : "CommodityDomain",
+                        //"group_name":"fx_selector_1_group",
+                        "elements": [
+                            {
+                                "value": true,
+                                "id": "fx_selector_1_rb1",
+                                "label": "Agricultural",
+                                "code": 1,
+                                "position": 0
+                            },
+                            {"value": false, "id": "fx_selector_1_rb2", "label": "Biofuels", "code": 2, "position": 1},
+                            {"value": false, "id": "fx_selector_1_rb3", "label": "Both", "code": -1, "position": 3}
+                        ],
+                        "lang": "EN",
+                        "title": {
+                            "EN": "Component 1",
+                            "ES": "ES List",
+                            "FR": "FR List"
+                        },
+                        "subtitle": {
+                            "EN": "SubTitle1EN",
+                            "ES": "SubTitle1ES",
+                            "FR": "SubTitle1FR"
+                        },
+                        "multipleselection": true,
+                        "name": "FirstComponent",
+                        "details": {
+                            "cl": {
+                                "system": "CS_Units",
+                                "version": "1.0"
+                            }
+                        },
+                        "component": {
+                            "source": {
+                                "datafields": [
+                                    {
+                                        "name": "label"
+                                    },
+                                    {
+                                        "name": "value"
+                                    }
+                                ],
+                                "id": "code"
+                            },
+                            "rendering": {
+                                "displayMember": "label",
+                                "valueMember": "value",
+                                "multiple": true,
+                                "width": "100%",
+                                "height": "100%"
+                            }
+                        }
+                    }
+                    ,
+                    {
+                        "componentType": "component1",
+                        "source": [
+                            {"label": "l6", "value": "v6"},
+                            {"label": "l7", "value": "v7"}
+                        ],
+                        //"type":"radiobuttongroup",
+                        //"domain_type" : "CommodityDomain",
+                        //"group_name":"fx_selector_1_group",
+                        "elements": [
+                            {
+                                "value": true,
+                                "id": "fx_selector_1_rb1",
+                                "label": "Agricultural",
+                                "code": 1,
+                                "position": 0
+                            },
+                            {"value": false, "id": "fx_selector_1_rb2", "label": "Biofuels", "code": 2, "position": 1},
+                            {"value": false, "id": "fx_selector_1_rb3", "label": "Both", "code": -1, "position": 3}
+                        ],
+                        "lang": "EN",
+                        "title": {
+                            "EN": "Component 2",
+                            "ES": "ES List",
+                            "FR": "FR List"
+                        },
+                        "multipleselection": true,
+                        "name": "SecondComponent",
+                        "details": {
+                            "cl": {
+                                "system": "CS_Units",
+                                "version": "1.0"
+                            }
+                        },
+                        "component": {
+                            "source": {
+                                "datafields": [
+                                    {
+                                        "name": "label"
+                                    },
+                                    {
+                                        "name": "value"
+                                    }
+                                ],
+                                "id": "code"
+                            },
+                            "rendering": {
+                                "displayMember": "label",
+                                "valueMember": "value",
+                                "multiple": true,
+                                "width": "100%",
+                                "height": "100%"
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                "containerType": "container1",
+                "title": "Container2Title",
+                "components": [
+                    {
+                        "componentType": "component1",
+                        "source": [
+                            {"label": "l13", "value": "v13"},
+                            {"label": "l23", "value": "v23"}
+                        ],
+                        //"type":"radiobuttongroup",
+                        //"domain_type" : "CommodityDomain",
+                        //"group_name":"fx_selector_1_group",
+                        "elements": [
+                            {
+                                "value": true,
+                                "id": "fx_selector_1_rb1",
+                                "label": "Agricultural",
+                                "code": 1,
+                                "position": 0
+                            },
+                            {"value": false, "id": "fx_selector_1_rb2", "label": "Biofuels", "code": 2, "position": 1},
+                            {"value": false, "id": "fx_selector_1_rb3", "label": "Both", "code": -1, "position": 3}
+                        ],
+                        "lang": "EN",
+                        "title": {
+                            "EN": "Component 3",
+                            "ES": "ES List",
+                            "FR": "FR List"
+                        },
+                        "subtitle": {
+                            "EN": "SubTitle2EN",
+                            "ES": "SubTitle1ES",
+                            "FR": "SubTitle1FR"
+                        },
+                        "multipleselection": true,
+                        "name": "ThirdComponent",
+                        "component": {
+                            "source": {
+                                "datafields": [
+                                    {
+                                        "name": "label"
+                                    },
+                                    {
+                                        "name": "value"
+                                    }
+                                ],
+                                "id": "code"
+                            },
+                            "rendering": {
+                                "displayMember": "label",
+                                "valueMember": "value",
+                                "multiple": true,
+                                "width": "100%",
+                                "height": "100%"
+                            }
+                        }
+                    }
+                    ,
+                    {
+                        "componentType": "component1",
+                        "source": [
+                            {"label": "l61", "value": "v61"},
+                            {"label": "l71", "value": "v71"}
+                        ],
+                        //"type":"radiobuttongroup",
+                        //"domain_type" : "CommodityDomain",
+                        //"group_name":"fx_selector_1_group",
+                        "elements": [
+                            {
+                                "value": true,
+                                "id": "fx_selector_1_rb1",
+                                "label": "Agricultural",
+                                "code": 1,
+                                "position": 0
+                            },
+                            {"value": false, "id": "fx_selector_1_rb2", "label": "Biofuels", "code": 2, "position": 1},
+                            {"value": false, "id": "fx_selector_1_rb3", "label": "Both", "code": -1, "position": 3}
+                        ],
+                        "lang": "EN",
+                        "title": {
+                            "EN": "Component 4",
+                            "ES": "ES List",
+                            "FR": "FR List"
+                        },
+                        "multipleselection": true,
+                        "name": "4Component",
+                        "component": {
+                            "source": {
+                                "datafields": [
+                                    {
+                                        "name": "label"
+                                    },
+                                    {
+                                        "name": "value"
+                                    }
+                                ],
+                                "id": "code"
+                            },
+                            "rendering": {
+                                "displayMember": "label",
+                                "valueMember": "value",
+                                "multiple": true,
+                                "width": "100%",
+                                "height": "100%"
+                            }
+                        }
+                    }
+                ]
+            }
+        ];
+        return modules;
+    };
 
     Application.prototype.initAnimation = function () {
 
@@ -90,7 +374,6 @@ define([
                 height: "100%",
                 ease: Power2.easeInOut,
                 onComplete: function () {
-                    console.log("on complete open the catalog")
                     $('.overlay-content').fadeIn('fast');
                 }
             });
@@ -98,10 +381,15 @@ define([
 
     Application.prototype.closeOverlay = function () {
         $('.overlay-content').fadeOut("fast", function () {
-            console.log("fadeout fast")
 
             $('.overlay-content').hide();
-            TweenLite.to($("#overlay"), 1, { width: "0%", height: "0%", ease: Power2.easeInOut});
+            TweenLite.to($("#overlay"), 1, {
+                width: "0%", height: "0%", ease: Power2.easeInOut, onComplete: function () {
+
+                    $(s.FILTER_HOLDER).hide();
+                    $(s.CATALOG_CONTAINER).show();
+                }
+            });
 
         })
     };
@@ -113,7 +401,7 @@ define([
         // Object { code_ITEM=[2], code_FLAG=[1], year_TIME=[2]}
         $('.overlay-content').fadeOut("fast", function () {
             $('.overlay-content').hide();
-            TweenLite.to($("#overlay"), 1, { width: "0%", height: "0%", ease: Power2.easeInOut});
+            TweenLite.to($("#overlay"), 1, {width: "0%", height: "0%", ease: Power2.easeInOut});
             //that.getData(payload, $.proxy(that.addItemToDesk, that));
 //            that.showFilter();
 
--- scripts/mains/analysis.js
+++ scripts/mains/analysis.js
@@ -4,27 +4,23 @@ require([
     '../../submodules/fenix-ui-catalog/js/paths',
     '../../submodules/fenix-ui-analysis/js/paths',
    '../../submodules/fenix-ui-filter/src/js/paths',
-    '../../submodules/fenix-ui-common/js/Compiler'
-], function (Catalog, Analysis, Filter, Compiler) {
-//], function (Catalog, Analysis, Compiler) {
-
-
-    var override = {
-        "fenix-ui-topmenu": '../components/fenix-ui-topmenu'
-    };
+    '../../submodules/fenix-ui-common/js/Compiler',
+    '../../submodules/fenix-ui-menu/js/paths'
+], function (Catalog, Analysis, Filter, Compiler, Menu) {
 
     var catalogConfig = Catalog;
     catalogConfig['baseUrl'] = '../../submodules/fenix-ui-catalog/js';
 
     var analysisConfig = Analysis;
     analysisConfig['baseUrl'] = '../../submodules/fenix-ui-analysis/js/';
-    analysisConfig['override'] = override;
 
     var filtersConfig = Filter;
     filtersConfig['baseUrl'] = '../../submodules/fenix-ui-filter/';
-    filtersConfig['override'] = override;
 
-    Compiler.resolve([catalogConfig, analysisConfig, filtersConfig],{
+    var menuConfig = Menu;
+    menuConfig['baseUrl'] = '../../submodules/fenix-ui-menu/js';
+
+    Compiler.resolve([catalogConfig, analysisConfig, filtersConfig, menuConfig],{
         placeholders:  {"FENIX_CDN": "//fenixapps.fao.org/repository"},
         config: {
 
@@ -33,6 +29,7 @@ require([
 
             // Specify the paths of vendor libraries
             paths: {
+                config : '../config',
                 'amplify' : '{FENIX_CDN}/js/amplify/1.1.2/amplify.min'
             },
 
@@ -48,40 +45,12 @@ require([
             //, urlArgs: 'bust=' +  (new Date()).getTime()
         }
     });
-//    Compiler.resolve([catalogConfig, analysisConfig], {"FENIX_CDN": "//fenixapps.fao.org/repository"});
 
-    require(["fenix-ui-topmenu/main", "../fx_analysis/application"], function (TopMenu, Application) {
-
-        new TopMenu({
-            url: 'json/fenix-ui-topmenu_config.json', active: "analysis"
-        });
+    require([ "../fx_analysis/application"], function ( Application) {
 
         var app = new Application();
 
         app.init();
-
-//        require(['fx-ana/start', 'fx-cat-br/start', 'fx-filter/start'],
-////        require(['fx-ana/start', 'fx-cat-br/start'],
-//            function (Analysis, Catalog, Filter) {
-//
-//                //new Filter ( your config)
-//
-//                console.log("Before filter2!!!")
-//            new Analysis().init({
-//                catalog: new Catalog({
-//                    catalog: {
-//                        BLANK_FILTER: 'config/submodules/catalog/uae-catalog-blank-filter.json'
-//                    },
-//                    results: {
-//                        actions: {
-//                            EDIT_METADATA: {}
-//                        }
-//                    }
-//                }),
-//                filter: new Filter().init()
-//            });
-//
-//        }); //end require FENIX component
 
     });  //end require Top Menu
 });
