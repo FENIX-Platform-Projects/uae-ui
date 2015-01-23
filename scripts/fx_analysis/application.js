591dc32773e5ab7642170bdd5fc0a3215b046021
danielefenix <daniele.salvatore@fao.org>
2015-01-23 17:51:52 +0100

    .

--- scripts/fx_analysis/application.js
+++ scripts/fx_analysis/application.js
@@ -5,9 +5,10 @@ define([
     'fx-ana/start',
     'fx-cat-br/start',
     'fx-filter/start',
+    'fx-filter/utils',
     'fx-menu/start',
     'amplify'
-], function ($, Analysis, Catalog, Filter, Menu) {
+], function ($, Analysis, Catalog, Filter, FilterUtils, Menu) {
 
     'use strict';
 
@@ -38,7 +39,9 @@ define([
         this.analysis = new Analysis().init();
 
         this.catalog = new Catalog().init({
-            BLANK_FILTER: 'config/submodules/catalog/uae-catalog-blank-filter.json',
+            bridge: {
+                blankFilter : 'config/submodules/catalog/uae-catalog-blank-filter.json'
+            },
             container: document.querySelector(s.CATALOG_CONTAINER),
             results: {
                 actions: {
@@ -67,8 +70,12 @@ define([
     };
 
     Application.prototype.addToFilter = function (resource) {
+
         console.log(resource)
-        //this.filter.add(this.getFilterConfiguration(resource));
+
+        console.log(FilterUtils.createConfiguration(resource))
+
+        this.filter.add(FilterUtils.createConfiguration(resource));
     };
 
 
@@ -77,6 +84,7 @@ define([
         amplify.subscribe('fx.widget.catalog.open', this, function (resource) {
 
             $(s.CATALOG_CONTAINER).hide();
+
             this.analysis.getData(resource, $.proxy(this.addToFilter, this))
 
         });
@@ -109,245 +117,6 @@ define([
 
     Application.prototype.filterResource = function () {
        console.log(this.filter.getValues())
-    };
-
-    Application.prototype.getFilterConfiguration = function (resource) {
-
-        var modules = [
-            {
-                "containerType": "container1",
-                "title": "Container1Title",
-                "components": [
-                    {
-                        "componentType": "component1",
-//                    "source": [
-//                        {"label": "l1", "value": "v1"},
-//                        {"label": "l2", "value": "v2"}
-//                    ],
-                        //"type":"radiobuttongroup",
-                        //"domain_type" : "CommodityDomain",
-                        //"group_name":"fx_selector_1_group",
-                        "elements": [
-                            {
-                                "value": true,
-                                "id": "fx_selector_1_rb1",
-                                "label": "Agricultural",
-                                "code": 1,
-                                "position": 0
-                            },
-                            {"value": false, "id": "fx_selector_1_rb2", "label": "Biofuels", "code": 2, "position": 1},
-                            {"value": false, "id": "fx_selector_1_rb3", "label": "Both", "code": -1, "position": 3}
-                        ],
-                        "lang": "EN",
-                        "title": {
-                            "EN": "Component 1",
-                            "ES": "ES List",
-                            "FR": "FR List"
-                        },
-                        "subtitle": {
-                            "EN": "SubTitle1EN",
-                            "ES": "SubTitle1ES",
-                            "FR": "SubTitle1FR"
-                        },
-                        "multipleselection": true,
-                        "name": "FirstComponent",
-                        "details": {
-                            "cl": {
-                                "system": "CS_Units",
-                                "version": "1.0"
-                            }
-                        },
-                        "component": {
-                            "source": {
-                                "datafields": [
-                                    {
-                                        "name": "label"
-                                    },
-                                    {
-                                        "name": "value"
-                                    }
-                                ],
-                                "id": "code"
-                            },
-                            "rendering": {
-                                "displayMember": "label",
-                                "valueMember": "value",
-                                "multiple": true,
-                                "width": "100%",
-                                "height": "100%"
-                            }
-                        }
-                    }
-                    ,
-                    {
-                        "componentType": "component1",
-                        "source": [
-                            {"label": "l6", "value": "v6"},
-                            {"label": "l7", "value": "v7"}
-                        ],
-                        //"type":"radiobuttongroup",
-                        //"domain_type" : "CommodityDomain",
-                        //"group_name":"fx_selector_1_group",
-                        "elements": [
-                            {
-                                "value": true,
-                                "id": "fx_selector_1_rb1",
-                                "label": "Agricultural",
-                                "code": 1,
-                                "position": 0
-                            },
-                            {"value": false, "id": "fx_selector_1_rb2", "label": "Biofuels", "code": 2, "position": 1},
-                            {"value": false, "id": "fx_selector_1_rb3", "label": "Both", "code": -1, "position": 3}
-                        ],
-                        "lang": "EN",
-                        "title": {
-                            "EN": "Component 2",
-                            "ES": "ES List",
-                            "FR": "FR List"
-                        },
-                        "multipleselection": true,
-                        "name": "SecondComponent",
-                        "details": {
-                            "cl": {
-                                "system": "CS_Units",
-                                "version": "1.0"
-                            }
-                        },
-                        "component": {
-                            "source": {
-                                "datafields": [
-                                    {
-                                        "name": "label"
-                                    },
-                                    {
-                                        "name": "value"
-                                    }
-                                ],
-                                "id": "code"
-                            },
-                            "rendering": {
-                                "displayMember": "label",
-                                "valueMember": "value",
-                                "multiple": true,
-                                "width": "100%",
-                                "height": "100%"
-                            }
-                        }
-                    }
-                ]
-            },
-            {
-                "containerType": "container1",
-                "title": "Container2Title",
-                "components": [
-                    {
-                        "componentType": "component1",
-                        "source": [
-                            {"label": "l13", "value": "v13"},
-                            {"label": "l23", "value": "v23"}
-                        ],
-                        //"type":"radiobuttongroup",
-                        //"domain_type" : "CommodityDomain",
-                        //"group_name":"fx_selector_1_group",
-                        "elements": [
-                            {
-                                "value": true,
-                                "id": "fx_selector_1_rb1",
-                                "label": "Agricultural",
-                                "code": 1,
-                                "position": 0
-                            },
-                            {"value": false, "id": "fx_selector_1_rb2", "label": "Biofuels", "code": 2, "position": 1},
-                            {"value": false, "id": "fx_selector_1_rb3", "label": "Both", "code": -1, "position": 3}
-                        ],
-                        "lang": "EN",
-                        "title": {
-                            "EN": "Component 3",
-                            "ES": "ES List",
-                            "FR": "FR List"
-                        },
-                        "subtitle": {
-                            "EN": "SubTitle2EN",
-                            "ES": "SubTitle1ES",
-                            "FR": "SubTitle1FR"
-                        },
-                        "multipleselection": true,
-                        "name": "ThirdComponent",
-                        "component": {
-                            "source": {
-                                "datafields": [
-                                    {
-                                        "name": "label"
-                                    },
-                                    {
-                                        "name": "value"
-                                    }
-                                ],
-                                "id": "code"
-                            },
-                            "rendering": {
-                                "displayMember": "label",
-                                "valueMember": "value",
-                                "multiple": true,
-                                "width": "100%",
-                                "height": "100%"
-                            }
-                        }
-                    }
-                    ,
-                    {
-                        "componentType": "component1",
-                        "source": [
-                            {"label": "l61", "value": "v61"},
-                            {"label": "l71", "value": "v71"}
-                        ],
-                        //"type":"radiobuttongroup",
-                        //"domain_type" : "CommodityDomain",
-                        //"group_name":"fx_selector_1_group",
-                        "elements": [
-                            {
-                                "value": true,
-                                "id": "fx_selector_1_rb1",
-                                "label": "Agricultural",
-                                "code": 1,
-                                "position": 0
-                            },
-                            {"value": false, "id": "fx_selector_1_rb2", "label": "Biofuels", "code": 2, "position": 1},
-                            {"value": false, "id": "fx_selector_1_rb3", "label": "Both", "code": -1, "position": 3}
-                        ],
-                        "lang": "EN",
-                        "title": {
-                            "EN": "Component 4",
-                            "ES": "ES List",
-                            "FR": "FR List"
-                        },
-                        "multipleselection": true,
-                        "name": "4Component",
-                        "component": {
-                            "source": {
-                                "datafields": [
-                                    {
-                                        "name": "label"
-                                    },
-                                    {
-                                        "name": "value"
-                                    }
-                                ],
-                                "id": "code"
-                            },
-                            "rendering": {
-                                "displayMember": "label",
-                                "valueMember": "value",
-                                "multiple": true,
-                                "width": "100%",
-                                "height": "100%"
-                            }
-                        }
-                    }
-                ]
-            }
-        ];
-        return modules;
     };
 
     Application.prototype.initAnimation = function () {
