116362f7c20cf65a9e064d16a3860900c3566cdd
danielefenix <daniele.salvatore@fao.org>
2015-06-15 18:03:36 +0200

    Tab chart creation

--- config/submodules/analysis/services.js
+++ config/submodules/analysis/services.js
@@ -8,7 +8,7 @@ define([], function () {
 
     var services = {
 
-        SERVICES_BASE_ADDRESS : 'http://fenix.fao.org/d3s_fenix/msd'
+        SERVICES_BASE_ADDRESS : 'http://fenix.fao.org/d3s_fenix'
         
     };
 
